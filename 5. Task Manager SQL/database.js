1d65ffbfbb40737ac0b59920cba5804206118b2e
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2024-10-07 10:23:07 +0200

    Refactor SQL questionnaire code, add Task Manager SQL files, and update database connection in database.js

--- 5. Task Manager SQL/database.js
+++ 5. Task Manager SQL/database.js
@@ -1,13 +1,65 @@
-const { 
-    createPool
-} = require('mysql');
 
-const pool = createPool({
-    host: "localhost",
-    user: "BigFudge",
-    password: "DatabaseManager2024",
-    database: "task_manager",
-    connectionLimit: 10
-})
+const mysql = require('mysql');
+const initialConnection = mysql.createConnection({
+  host: "localhost",
+  user: "BigFudge",
+  password: "DatabaseManager2024"
+});
 
-module.exports = pool;
+function checkDatabase() {
+    return new Promise((resolve, reject) => {
+        initialConnection.connect((err) => {
+            if (err) {
+                console.error("Error connecting to MySQL server:", err.message);
+                return reject(err);
+            }
+            console.log("Connected to MySQL server.");
+
+            const dbName = 'task_manager';
+            initialConnection.query(`CREATE DATABASE IF NOT EXISTS ${dbName}`, (err) => {
+                if (err) {
+                    console.error("Error creating database:", err.message);
+                    initialConnection.end();
+                    return reject(err);
+                }
+                console.log(`Database "${dbName}" checked or created successfully.`);
+
+                initialConnection.query(`USE ${dbName}`, (err) => {
+                    if (err) {
+                        console.error("Error selecting database:", err.message);
+                        initialConnection.end();
+                        return reject(err);
+                    }
+                    console.log(`Database "${dbName}" selected.`);
+
+                    const createTableQuery = `
+                        CREATE TABLE IF NOT EXISTS tasks (
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            task VARCHAR(255) NOT NULL,
+                            status VARCHAR(10) DEFAULT 'To do'
+                        )`;
+                    initialConnection.query(createTableQuery, (err) => {
+                        if (err) {
+                            console.error("Error creating table:", err.message);
+                            initialConnection.end();
+                            return reject(err);
+                        }
+                        console.log("Table 'tasks' checked or created successfully.");
+                        initialConnection.end();
+                        resolve();
+                    });
+                });
+            });
+        });
+    });
+}
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "BigFudge",
+  password: "DatabaseManager2024",
+  database: "task_manager",
+  connectionLimit: 10,
+});
+
+module.exports = { db: pool, checkDatabase };
--- 5. Task Manager SQL/index.js
+++ 5. Task Manager SQL/index.js
@@ -1,13 +1,9 @@
 const readline = require("readline");
-const fs = require("fs");
-const { log } = require("console");
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
 });
-
-const db = require('./database');
-
+const { checkDatabase, db } = require("./database");
 
 const welcome_message = `
 -------------------------------
@@ -16,143 +12,286 @@ Welcome to your task manager, Press:
 2. to add a task,
 3. to delete a task,
 4. to mark a task as done,
-5. to show tasks left to do,
-6. to Exit the task manager
+5. to mark a task as pending,
+6. to mark a task as to do,
+7. to show tasks left to do,
+8. to Exit the task manager
 -------------------------------`;
 
-const data = require("./data.json") || [];
-
-function mainMenu() {
-	console.log(welcome_message);
-
-	rl.question("Your choice: ", (answer) => {
-    handleUserChoice(answer)
-	});
-}
-
-function handleUserChoice(option) {
-  switch (option) {
-    case "1":
-      display_tasks();
-      mainMenu();
-      break;
-
-    case "2":
-      add_task();
-      break;
-
-    case "3":
-      delete_task();
-      break;
-
-    case "4":
-      task_done();
-      break;
-
-    case "5":
-      filter_task();
-      mainMenu();
-      break;
-
-    case "6":
-      rl.close();
-      break;
-
-    default:
-      invalid_answer();
-      break;
-  }
-}
-
-function display_tasks() {
-	db.query('select*from tasks', (err, result) => { 
-		if (err) throw err;
-		console.log(result);
-	})
-}
-
-function add_task() {
-	rl.question("Enter your task: ", (answer) => {
-		db.query(`INSERT INTO tasks ${answer}`, (err) => { })
-
-
-
-
-
-
-		// const new_task = {
-		// 	id: data.length + 1,
-		// 	task: answer,
-		// 	status: false,
-		// };
-		// data.push(new_task);
-
-		// console.log("Task added successfully!");
-		// save_data();
-		// mainMenu();
-	});
-}
-
-function task_done() {
-	if (Object.keys(data).length != 0) {
-		rl.question("Enter the number of the task : ", (answer) => {
-			data[answer - 1].status = !data[answer - 1].status;
-
-			rl.question("Do you want to delete the task(y/n)", (decision) => {
-				if (decision === "y") {
-					data.splice(answer - 1, 1);
-
-					save_data();
-					mainMenu();
-				} else {
-					save_data();
-					mainMenu();
-				}
-			});
-		});
-	} else {
-		console.log("No task here");
+async function main() {
+	try {
+		console.log("-------------------------------");
+		await checkDatabase();
+		console.log("-------------------------------");
+	} 
+	catch (err) {
+		console.error(err);
+	}
+	finally{ 
 		mainMenu();
 	}
 }
 
-function delete_task() {
-	if (Object.keys(data).length != 0) {
-		display_tasks();
-
-		rl.question(`Enter the number of the task to delete : `, (answer) => {
-			if (parseInt(answer) > Object.keys(data).length) {
-				invalid_answer();
-				mainMenu();
+function getAllTasks() {
+	return new Promise((resolve, reject) => {
+		db.query("SELECT * FROM tasks", (err, result) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(result);
 			}
-			if (data.length === 0) {
-				log("Nothing to delete");
-				mainMenu();
-			}
-
-
-			data.splice(answer - 1, 1);
-			save_data();
-			mainMenu();
 		});
+	});
+}
+
+async function mainMenu() {
+	console.log(welcome_message);
+
+	rl.question("Your choice: ", (answer) => {
+		handleUserChoice(answer);
+	});
+}
+
+async function handleUserChoice(option) {
+	switch (option) {
+		case "1":
+			await display_tasks();
+			mainMenu();
+			break;
+
+		case "2":
+			add_task();
+			break;
+
+		case "3":
+			delete_task();
+			break;
+
+		case "4":
+			task_done();
+			break;
+
+		case "5":
+			task_pending();
+			break;
+
+		case "6":
+			task_todo();
+			break;
+
+		case "7":
+			await filter_task();
+			break;
+
+		case "8":
+			process.exit(0);
+			break;
+
+		default:
+			invalid_answer();
+			mainMenu();
+			break;
 	}
 }
 
-function filter_task() {
-	data.forEach((task) => {
-		if (task.status === false) {
-			console.log(`${task.id} : ${task.task}`);
-		}
+async function display_tasks() {
+	const tasks = await getAllTasks();
+
+	let index = 1;
+	console.log("-------------------------------");
+	tasks.forEach((task) => {
+		console.log(`${index}. Description: ${task.task}, Status: ${task.status}`);
+		index++;
 	});
-	mainMenu();
 }
 
-function save_data() {
-	fs.writeFileSync("./data.json", JSON.stringify(data, null, 2));
+async function add_task() {
+	rl.question("Enter your task: ", async (answer) => {
+		try {
+			await new Promise((resolve, reject) => {
+				db.query(`INSERT INTO tasks (task) VALUES (?)`, [answer], (err) => {
+					if (err) {
+						console.error("Error adding task:", err.message);
+						reject(err);
+					} else {
+						console.log("Task added");
+						resolve();
+					}
+				});
+			});
+		} catch (err) {
+			console.error("Failed to add task:", err.message);
+		} finally {
+			mainMenu();
+		}
+	});
+}
+
+async function delete_task() {
+	const tasks = await getAllTasks();
+	await display_tasks();
+
+	rl.question("Enter the number of the task to delete: ", async (answer) => {
+		const taskIndex = parseInt(answer) - 1;
+		if (isNaN(taskIndex) || taskIndex < 0 || taskIndex >= tasks.length) {
+			console.log("Invalid index. Please select a valid index.");
+			mainMenu();
+			return;
+		}
+		const taskIdToDelete = tasks[taskIndex].id;
+
+		try {
+			await new Promise((resolve, reject) => {
+				db.query("DELETE FROM tasks WHERE id = ?", [taskIdToDelete], (err, result) => {
+					if (err) {
+						console.error("Error deleting task:", err.message);
+						reject(err);
+					} else if (result.affectedRows === 0) {
+						console.log("No task found with the given ID.");
+						resolve();
+					} else {
+						console.log("Task deleted successfully.");
+						resolve();
+					}
+				});
+			});
+		} catch (err) {
+			console.error("Failed to delete task:", err.message);
+		} finally {
+			mainMenu();
+		}
+	});
+}
+
+async function task_done() {
+	const tasks = await getAllTasks();
+	await display_tasks();
+
+	rl.question("Enter the number of the task to mark as done: ", async (answer) => {
+		const taskIndex = parseInt(answer) - 1;
+		if (isNaN(taskIndex) || taskIndex < 0 || taskIndex >= tasks.length) {
+			console.log("Invalid index. Please select a valid index.");
+			mainMenu();
+			return;
+		}
+		const taskIdToMark = tasks[taskIndex].id;
+		try {
+			await new Promise((resolve, reject) => {
+				db.query("UPDATE tasks SET status = ? WHERE id = ?", ["Done", taskIdToMark], (err, result) => {
+					if (err) {
+						console.error("Error marking task as done:", err.message);
+						reject(err);
+					} else if (result.affectedRows === 0) {
+						console.log("No task found with the given ID.");
+						resolve();
+					} else {
+						console.log("Task marked as done.");
+						resolve();
+					}
+				});
+			});
+		} catch (err) {
+			console.error("Failed to mark task as done:", err.message);
+		} finally {
+			mainMenu();
+		}
+	});
+}
+
+async function task_pending() {
+	const tasks = await getAllTasks();
+	await display_tasks();
+
+	rl.question("Enter the number of the task to mark as pending : ", async (answer) => {
+		const taskIndex = parseInt(answer) - 1;
+		if (isNaN(taskIndex) || taskIndex < 0 || taskIndex >= tasks.length) {
+			console.log("Invalid index. Please select a valid index.");
+			mainMenu();
+			return;
+		}
+		const taskIdToMark = tasks[taskIndex].id;
+		try {
+			await new Promise((resolve, reject) => {
+				db.query("UPDATE tasks SET status = ? WHERE id = ?", ["Pending", taskIdToMark], (err, result) => {
+					if (err) {
+						console.error("Error marking task as Pending :", err.message);
+						reject(err);
+					} else if (result.affectedRows === 0) {
+						console.log("No task found with the given Index.");
+						resolve();
+					} else {
+						console.log("Task marked as Pending.");
+						resolve();
+					}
+				});
+			});
+		} catch (err) {
+			console.error("Failed to mark task as Pending:", err.message);
+		} finally {
+			mainMenu();
+		}
+	});
+}
+
+async function task_todo() {
+	const tasks = await getAllTasks();
+	await display_tasks();
+
+	rl.question("Enter the number of the task to mark as To do: ", async (answer) => {
+		const taskIndex = parseInt(answer) - 1;
+		if (isNaN(taskIndex) || taskIndex < 0 || taskIndex >= tasks.length) {
+			console.log("Invalid index. Please select a valid index.");
+			mainMenu();
+			return;
+		}
+		const taskIdToMark = tasks[taskIndex].id;
+		try {
+			await new Promise((resolve, reject) => {
+				db.query("UPDATE tasks SET status = ? WHERE id = ?", ["To do", taskIdToMark], (err, result) => {
+					if (err) {
+						console.error("Error marking task as To do :", err.message);
+						reject(err);
+					} else if (result.affectedRows === 0) {
+						console.log("No task found with the given Index.");
+						resolve();
+					} else {
+						console.log("Task marked as To do.");
+						resolve();
+					}
+				});
+			});
+		} catch (err) {
+			console.error("Failed to mark task as To Do :", err.message);
+		} finally {
+			mainMenu();
+		}
+	});
+}
+
+async function filter_task() {
+	try {
+		const tasks = await new Promise((resolve, reject) => {
+			db.query("SELECT * FROM tasks WHERE status = 0", (err, result) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(result);
+				}
+			});
+		});
+
+		tasks.forEach((task) => {
+			console.log(`Task ID: ${task.id}, Description: ${task.task}`);
+		});
+	} catch (err) {
+		console.error("Error filtering tasks:", err.message);
+	} finally {
+		mainMenu();
+	}
 }
 
 function invalid_answer() {
 	console.log("Not a valid answer");
 }
 
-mainMenu();
+main();
